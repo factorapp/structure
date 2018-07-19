7fa7f77cbe148239247f4f084c515d2c088285d5
Brian Ketelsen <bketelsen@gmail.com>
2018-07-19 17:48:12 -0400

    more

--- reconciler.go
+++ reconciler.go
@@ -4,6 +4,8 @@ type Reconciler interface {
 	Reconcile(c Controller)
 }
 type DOMReconciler struct {
+	sources map[string]Element
+	target  map[string]Element
 }
 
 func (d *DOMReconciler) Reconcile(c Controller) {
