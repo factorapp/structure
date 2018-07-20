ca836d38490644a3e3390e7ddd00f5e1e026bd87
Aaron Schlesinger <arschles@gmail.com>
2018-07-20 16:19:49 -0700

    starting to add a server

--- go.mod
+++ go.mod
@@ -1,6 +1,9 @@
 module github.com/factorapp/structure
 
 require (
+	github.com/gobuffalo/buffalo v0.12.4
+	github.com/gobuffalo/packr v1.11.1
+	github.com/gorilla/mux v1.6.2
 	github.com/gowasm/go-js-dom v0.0.1
 	github.com/zmb3/gogetdoc v0.0.0-20180522031303-10095872a7c5 // indirect
 	golang.org/x/tools v0.0.0-20180720055721-99195f4d4ffa // indirect
---
+++ server/server.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/packr"
+)
+
+type Server struct {
+	hdl http.Handler
+}
+
+func (s *Server) Start(port int) error {
+	return http.ListenAndServe(":"+strconv.Itoa(port), s.hdl)
+}
+
+func NewServer(templates packr.Box) http.Handler {
+	app := buffalo.New(buffalo.Options{})
+	app.GET("/wasm_exec.js", wasmJS)
+	handler := mux.NewRouter()
+	handler.HandleFunc
+	src := http.Server{}
+	// TODO: this thing needs to serve the wasm app, the wasm.js
+	return &Server{}
+}
