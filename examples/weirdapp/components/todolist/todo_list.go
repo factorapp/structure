71cf87580bf2b08048a165693c061d136649eb58
Aaron Schlesinger <arschles@gmail.com>
2018-07-21 20:48:45 -0700

    making todo list do more stuff

--- examples/weirdapp/components/todolist/todo_list.go
+++ examples/weirdapp/components/todolist/todo_list.go
@@ -16,10 +16,39 @@ type TodoList struct {
 	Todos []Todo
 }
 
-func (t *TodoList) Add(ctx core.Context) {
+func (t *TodoList) Add(ctx core.Context) error {
+	nameElt, err := ctx.FormInput("name")
+	if err != nil {
+		return err
+	}
+	descriptionElt, err := ctx.FormInput("description")
+	if err != nil {
+		return err
+	}
+	name := nameElt.Value()
+	description := descriptionElt.Value()
+
+	pretty.Println("name", name)
+	pretty.Println("description", description)
+
+	// tpl, err := ctx.ParseTemplate("todo")
+	// if err != nil {
+	// 	return err
+	// }
+	// if err := tpl.Render("div#todos", map[string]interface{}{
+	// 	"name": name,
+	// 	"description": description
+	// }); err != nil {
+	// 	return err
+	// }
+	return nil
 	// TODO: how do you pass in form data?
-	name := t.Targets()["name"][0]
-	pretty.Println("name:", name)
+	// name := t.Targets()["name"][0]
+	// pretty.Println("name:", name.TagName())
+
+	// name, err := t.Input("name")
+	// pretty.Println("error:", err)
+	// pretty.Println("name:", name)
 
 	// // TODO: event handlers return errors?
 	// name, _ := t.GetFormData("name")
